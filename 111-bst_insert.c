b4f5e702af74077f2df85b72be653c3e4912ba07
amira-creator <amira.abdelfattah99@gmail.com>
2023-10-04 13:16:40 -0700

    S

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -25,4 +25,18 @@ bst_t *bst_insert(bst_t **tree, int value)
 		else if (value == tmp->n)
 			return (NULL);
 	}
+	new_node = binary_tree_node(NULL, value);
+	if (tmp_2 == NULL)
+		tmp_2 = new_node;
+	else if (value < tmp_2->n)
+	{
+		tmp_2->left = new_node;
+		new_node->parent = tmp_2;
+	}
+	else
+	{
+		tmp_2->right = new_node;
+		new_node->parent = tmp_2;
+	}
+	return (new_node);
 }
