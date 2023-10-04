314bde442b09dd055b0f405865031c967c4db603
amira-creator <amira.abdelfattah99@gmail.com>
2023-10-04 13:05:16 -0700

    D

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -28,3 +28,33 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left, right;
+
+	if (tree == NULL)
+		return (0);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	if (left >= right)
+		return (1 + left);
+	return (1 + right);
+}
+
+/**
+ * binary_tree_level - Function that perform func on specific level of b-tree
+ * @tree: Pointer to the root of the tree
+ * @i: The level of the tree to perform a function on
+ * @func: The function
+ * Return: None
+*/
+void binary_tree_level(const binary_tree_t *tree, size_t i, void (*func)(int))
+{
+	if (tree == NULL)
+		return;
+	if (i == 1)
+		func(tree->n);
+	else if (i > 1)
+	{
+		binary_tree_level(tree->left, i - 1, func);
+		binary_tree_level(tree->right, i - 1, func);
+	}
+}
